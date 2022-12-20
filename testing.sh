c3e9bf6a0bce5f738b20ddd8e972c6f7caec7cec
EnricoPezzano <enrico.pezzano@outlook.it>
2022-12-20 15:49:45 +0100

    enrico's script push

--- testing.sh
+++ testing.sh
@@ -17,6 +17,11 @@
 
 # TODO testa my-automated-mac-setup copiando ed incollando un pezzo alla volta
 
-# https://youtube.com/watch?v=TlT7xjM01k8&feature=share
-
 # https://apple.stackexchange.com/questions/200691/set-finder-arranging-order-by-kind-via-terminal
+
+# TODO setta tutte le preferenze di sistema finder, safari, etc con la registrazione di automator
+# +
+# TODO manda notifica che dica di eseguire quel workflow dall’interno a 10x
+# i.e.:
+# echo -e "\033[1;31m First thing first: sending alert to execute automator workflow (at 10x) while the script keep running \033[0m"
+# alert "launch the app $name from Automator at 10x velocity to set some preferences across sys, safari and finder"
