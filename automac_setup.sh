74931eb7b028d98c168b40f4acae8ef287d0376f
EnricoPezzano <enrico.pezzano@outlook.it>
2024-03-12 14:38:22 +0100

    added mascottes

--- automac_setup.sh
+++ automac_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 # i think that the shebang is not necessary because I'm running this script with sh
 
-
+# 🍏🧃🍎 TODO to use as mascottes and success/error message
 # TODO: set "key repeat rate" to fast and "delay until repeat"" to short
 
 # defaults write /Library/Preferences/com.apple.loginwindow DesktopPicture "Put your desktop picture in here
